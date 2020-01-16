c40bdf7d5404fc3ff7c8a998e88d671c48f46ee2
geomon <1623537355@qq.com>
2020-01-16 16:27:31 +0800

    update proxy orchestration row-jdbc

--- sharding-proxy-example/proxy-orchestration-example/sharding-proxy-orchestration-raw-jdbc-postgresql-example/src/test/java/org/apache/shardingsphere/example/RawJDBCOrchestrationJavaEncryptTest.java
+++ sharding-proxy-example/proxy-orchestration-example/sharding-proxy-orchestration-raw-jdbc-postgresql-example/src/test/java/org/apache/shardingsphere/example/RawJDBCOrchestrationJavaEncryptTest.java
@@ -3,7 +3,7 @@ package org.apache.shardingsphere.example;
 import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
 import org.apache.shardingsphere.example.core.api.service.ExampleService;
 import org.apache.shardingsphere.example.core.jdbc.common.RawJdbcAssertUtils;
-import org.apache.shardingsphere.example.core.jdbc.repository.UserRepositoryImpl;
+import org.apache.shardingsphere.example.core.jdbc.repository.postgresql.UserRepositoryImpl;
 import org.apache.shardingsphere.example.core.jdbc.service.UserServiceImpl;
 import org.apache.shardingsphere.example.factory.DataSourceFactory;
 import org.apache.shardingsphere.example.type.ShardingType;
